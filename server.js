a6b4559d9f1600b38ed75c57450917ab3341bed8
Kira Selucky <kiraashley24@gmail.com>
2024-05-03 17:51:55 -0600

    404

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ app.get('/', (req, res) => {
 });
 // Apply the CORS middleware
 app.use(cors());
+
+//404
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
 // Middleware
 app.use(express.json());
 /*
