5bc777f3740f69fac346f49685a6e1a00d597535
Kira Selucky <kiraashley24@gmail.com>
2024-05-03 20:19:15 -0600

    changed back bc constantly deploys

--- server.js
+++ server.js
@@ -26,7 +26,13 @@ app.use('/states', require('./routes/states'));
 // Apply the CORS middleware
 app.use(cors());
 
-
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+   
+  app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+  })
 
 //404
 app.all('*', (req, res) => {
