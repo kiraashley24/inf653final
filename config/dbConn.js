34d8ea199ae8669a4bd1eb5352f1fc6e943b852a
Kira Selucky <kiraashley24@gmail.com>
2024-05-08 14:10:00 -0600

    adjusted parser error

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
+            
         });
+        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
     } catch (err) {
-        console.error(err);
+        console.error('COULD NOT CONNECT TO DATABASE:', err);
     }
 }
 
 module.exports = connectDB
+
